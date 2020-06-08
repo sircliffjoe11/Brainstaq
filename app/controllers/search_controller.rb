cffaf4fb274ebb22791e2187c47e6b515b35d735
Clifford Ettah <sircliffjoe11@gmail.com>
2020-06-09 00:48:33 +0100

    added search functionality

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,15 @@
 class SearchController < ApplicationController
-#   def index
-#     if params[:query].start_with?('#')
-#       query  = params[:query].gsub('#', '')
-#       @posts = Idea.joins(:idea).where(ideas: {title:    query})
-#     else
-#       @posts = Idea.where("description like ?", "%#{params[:query]}%")
-#     end
-#   end
+  def index
+    if params[:query].start_with?('#')
+      query  = params[:query].gsub('#', '')
+      @ideas = Idea.joins(:idea).where(ideas: {title:    query})
+    else
+      @ideas = Idea.where("title like ? or description like ?", "%#{params[:query]}%", "%#{params[:query]}%")
+    end
+  end
 
-#   def check_ideas_exists
-#     if Idea.exists?(:title => self.title)
-#     end
-#   end 
+  def check_ideas_exists
+    if Idea.exists?(:title => self.title)
+    end
+  end 
 end
