cc10bb7967f54244d053074f990346cd78490ab3
Clifford Ettah <sircliffjoe11@gmail.com>
2020-07-14 14:20:03 +0100

    Modification on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   
   has_one_attached :image, dependent: :destroy
 
-  after_create :send_admin_mail
+  #after_create :send_admin_mail
   # def send_admin_mail
   #   UserMailer.send_welcome_email(self).deliver_later
   # end
