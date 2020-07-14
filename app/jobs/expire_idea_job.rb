3fa4040a09c8e1bcc8b4e5f2f3993e01d4ed291e
Clifford Ettah <sircliffjoe11@gmail.com>
2020-07-14 13:48:09 +0100

    modified background jobs temporarily

--- app/jobs/expire_idea_job.rb
+++ app/jobs/expire_idea_job.rb
@@ -1,20 +1,20 @@
 class ExpireIdeaJob < ApplicationJob
-  queue_as :default
+  # queue_as :default
 
-  def perform(idea)
-    @idea = idea
+  # def perform(idea)
+  #   @idea = idea
 
-    return if idea_already_inactive?
+  #   return if idea_already_inactive?
 
-    @idea.status = "inactive"
-    @idea.save!
+  #   @idea.status = "inactive"
+  #   @idea.save!
 
-    UserMailer.with(idea: @idea).idea_expired_notice.deliver_later
-  end
+  #   UserMailer.with(idea: @idea).idea_expired_notice.deliver_later
+  # end
 
-  private 
+  # private 
 
-  def idea_already_inactive?
-    @idea.status == "inactive"
-  end
+  # def idea_already_inactive?
+  #   @idea.status == "inactive"
+  # end
 end
