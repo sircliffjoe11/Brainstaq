067541d07e839c4557db6c68b3b6013fa7a7346a
Clifford Ettah <sircliffjoe11@gmail.com>
2020-07-17 14:59:08 +0100

    Configured aws-sdk-3

--- Gemfile
+++ Gemfile
@@ -74,6 +74,7 @@ gem 'pg'
 
 gem "fog-aws"
 gem 'activerecord', '~> 6.0', '>= 6.0.3.2'
+gem "aws-sdk-s3", require: false
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,19 +1,3 @@
-const { environment } = require('@rails/webpacker');
-
-const webpack = require('webpack');
-
-environment.plugins.append("Provide", new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery',
-  Popper: ['popper.js', 'default'],
-  'window.jQuery': 'jquery'
-}));
-
-const aliasConfig = {
-  'jquery': 'jquery-ui-dist/external/jquery/jquery.js',
-  'jquery-ui': 'jquery-ui-dist/jquery-ui.js'
-};
-
-environment.config.set('resolve.alias', aliasConfig);
+const { environment } = require('@rails/webpacker')
 
 module.exports = environment
