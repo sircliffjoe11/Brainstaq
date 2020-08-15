34aca784052aafdcadc227e7d417f8408a18b8e5
Clifford Ettah <sircliffjoe11@gmail.com>
2020-08-15 23:22:10 +0100

    installed webpacker

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,18 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const { environment } = require('@rails/webpacker')
-// const environment = require('./environment')
-
-// environment.plugins.delete('UglifyJs')
-
-// environment.plugins.append(
-//   'UglifyJs',
-//   new UglifyJsPlugin({
-//     sourceMap: true
-//   })
-// )
-
-environment.config.set('optimization.minimizer', [new UglifyJsPlugin()])
+const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
