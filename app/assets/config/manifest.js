c7f26d5fc4c958152df3a76bd2223b8e0d1c8a03
Clifford Ettah <sircliffjoe11@gmail.com>
2020-08-15 12:50:24 +0100

    modified manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,8 @@
 //= link_tree ../images
-//= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+//= link_directory ../javascripts .js
+
+
 //= link intro/application.js
 //= link intro/application.css
 //= link intro/admin/application.css
