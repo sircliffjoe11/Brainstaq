d2ef96b341f98526cf18e50cfd57580c20697055
Clifford Ettah <sircliffjoe11@gmail.com>
2020-08-16 14:03:50 +0100

    modified user and ideas show views

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,8 +15,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("trix")
-require("@rails/actiontext")
 require("bootstrap")
 
 var jQuery = require("jquery");
