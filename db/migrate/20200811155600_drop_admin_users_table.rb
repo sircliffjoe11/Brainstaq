a10f59adee8a30dad0c52041afd824533fbff6f7
Clifford Ettah <sircliffjoe11@gmail.com>
2020-08-23 15:42:35 +0100

    modified db migrations

--- db/migrate/20200811155600_drop_admin_users_table.rb
+++
@@ -1,6 +0,0 @@
-class DropAdminUsersTable < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :admin_users
-    drop_table :active_admin_comments
-  end
-end
--- db/migrate/20200822155835_drop_campaigns_table.rb
+++
@@ -1,5 +0,0 @@
-class DropCampaignsTable < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :campaigns
-  end
-end
