15eb7fa7c893c8c937435effcf835a36ce0240d9
Clifford Ettah <sircliffjoe11@gmail.com>
2020-08-24 13:58:45 +0100

    modified email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   config.action_mailer.default charset: 'utf-8'
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
-    port: 587,
+    port: 465,
     domain: 'gmail.com',
     authentication: 'plain',
     enable_starttls_auto: true,
