a51031dd2f5f7096aecb4b5f4a413fcbc2a30f44
Clifford Ettah <sircliffjoe11@gmail.com>
2021-01-15 00:46:00 +0100

    modified yml file

--- config/initializers/new_google_recaptcha.rb
+++ config/initializers/new_google_recaptcha.rb
@@ -1,7 +1,7 @@
 if Object.const_defined?('NewGoogleRecaptcha')
   NewGoogleRecaptcha.setup do |config|
-    config.site_key   = "6LdMUysaAAAAAJIpOQKrY1u7Xkn1tvu4fprHPD0U"
-    config.secret_key = "6LdMUysaAAAAALrcw7Qk6p6Qs4-b_1UnQGY_PjxE"
+    config.site_key   = ENV["GOOGLE_RECAPTCHA_SITE_KEY"],
+    config.secret_key = ENV["GOOGLE_RECAPTCHA_SECRET_KEY"], 
     config.minimum_score = 0.5
   end
 end
