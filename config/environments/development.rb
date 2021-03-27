9bde7d6008590b97076fd1962274a43542b8399c
Clifford Ettah <sircliffjoe11@gmail.com>
2021-03-27 20:33:22 +0100

    added mobile responsiveness

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,5 +75,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  # config.hosts << "2a8272d6e55c.ngrok.io"
+  config.hosts << "e37c51da0813.ngrok.io"
 end
